53e54a2e2377a2f27eaefba24e569b4eb12a1517
Felix Alef <felixalef@gmail.com>
2021-11-06 13:36:41 -0300

    Desvio condicional simples

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IdadeJogador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte idade;
+            Console.WriteLine("Digite a idade do jogador");
+            idade = Convert.ToByte(Console.ReadLine());
+
+            if (idade >= 12)
+            {
+                Console.WriteLine("Voce pode Jogar!");
+            }
+            if (idade < 12)
+            {
+                Console.WriteLine("Voce não pode Jogar!");
+            }
+        }
+    }
+}
